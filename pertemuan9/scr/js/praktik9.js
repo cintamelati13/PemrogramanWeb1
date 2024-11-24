c23dbe6ce4348f38063c592c7ec367e543aa5b0f
cintamelati13 <cntmlt130@gmail.com>
2024-11-24 09:17:54 +0700

    Add files via upload

---
+++ pertemuan9/scr/js/praktik9.js
@@ -0,0 +1,38 @@
+// Soal 1
+function hitungLingkaran(){
+    var radius = document.getElementById("radius").value;
+    if (radius > 0){
+        var luas = 3.14 * radius * radius;
+        var keliling = 2 * 3.14 * radius;
+        document.getElementById("lingkaranOutput").
+        innerText = "Luas : " + "cm², keliling : "
+        + keliling + "cm²";
+    }else {
+        document.getElementById("lingkaranOutput").
+        innerText = "Masukan jari-jari yang valid!";
+    }
+}
+
+// Soal 2
+function hitungDuit(){
+    var rupiah = document.getElementById("rupiah").value;
+    if (rupiah > 0){
+        var kurs = 15870;
+        var dollar = rupiah / kurs;
+        document.getElementById("hasilDuit").innerText =
+        "Nilai : $ " + dollar.toFixed(2);
+    }else {
+        Document.getElementById("hasilDuit").innerText =
+        "Masukan Nilai yang Valid !!"
+    }
+}
+
+// Soal 3
+function hitungPenjumlahan(){
+    var bil1 = parseFloat(
+        document.getElementById("bil1").value) || 0;
+    var bil2 = parseFloat(
+        document.getElementById("bil2").value) || 0;
+    var jumlah = bil1 + bil2;
+    document.getElementById("hasilJumlah").innerText ="Jumlah: " + jumlah.toFixed(2);
+}
